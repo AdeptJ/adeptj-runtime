4ed91195a137f561d42b5076184d7dd9827ceb15
Rakesh Kumar <kumar_rakesh@live.in>
2016-10-08 09:20:11 +0530

    License header style updated.

--- src/main/java/com/adeptj/modularweb/micro/undertow/UndertowShutdownHook.java
+++ src/main/java/com/adeptj/modularweb/micro/undertow/UndertowShutdownHook.java
@@ -1,23 +1,22 @@
-/* 
- * =============================================================================
- * 
- * Copyright (c) 2016 AdeptJ
- * Copyright (c) 2016 Rakesh Kumar <irakeshk@outlook.com>
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * =============================================================================
- */
+/** 
+###############################################################################
+#                                                                             # 
+#    Copyright 2016, Rakesh Kumar, AdeptJ (http://adeptj.com)                 #
+#                                                                             #
+#    Licensed under the Apache License, Version 2.0 (the "License");          #
+#    you may not use this file except in compliance with the License.         #
+#    You may obtain a copy of the License at                                  #
+#                                                                             #
+#        http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                             #
+#    Unless required by applicable law or agreed to in writing, software      #
+#    distributed under the License is distributed on an "AS IS" BASIS,        #
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+#    See the License for the specific language governing permissions and      #
+#    limitations under the License.                                           #
+#                                                                             #
+###############################################################################
+*/
 package com.adeptj.modularweb.micro.undertow;
 
 import javax.servlet.ServletException;
@@ -31,7 +30,7 @@ import io.undertow.Undertow;
 import io.undertow.servlet.api.DeploymentManager;
 
 /**
- * ShutdownHook for graceful handling of CTRL+C, this first cleans up the deployment and then stops UNDERTOW server.
+ * ShutdownHook for graceful server shutdown, this first cleans up the deployment and then stops UNDERTOW server.
  * 
  * Rakesh.Kumar, AdeptJ
  */
