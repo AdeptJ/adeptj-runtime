5e8d70eadb33b51b5b929c52d5dfd67389494d38
Rakesh Kumar <kumar_rakesh@live.in>
2016-11-15 12:51:49 +0530

    minor update to log.

--- src/main/java/com/adeptj/runtime/osgi/FrameworkProvisioner.java
+++ src/main/java/com/adeptj/runtime/osgi/FrameworkProvisioner.java
@@ -97,7 +97,7 @@ public enum FrameworkProvisioner {
 				this.framework.stop();
 				// A value of zero will wait indefinitely.
 				FrameworkEvent event = this.framework.waitForStop(0);
-				logger.info("OSGi Framework Stopped, Event: [{}]", FrameworkEvents.value(event.getType()));
+				logger.info("OSGi FrameworkEvent: [{}]", FrameworkEvents.value(event.getType()));
 			} else {
         		logger.info("OSGi Framework not started yet, nothing to stop!!");
         	}
