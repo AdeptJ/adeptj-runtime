1a8ae17186b70e9e0f4a836e9635a4c2ac8307e6
Rakesh Kumar <kumar_rakesh@live.in>
2016-11-18 07:59:48 +0530

    TemplateEngine configs moved to provisioning file, better exception handling while processing view.

--- src/main/java/com/adeptj/runtime/viewengine/ViewEngine.java
+++ src/main/java/com/adeptj/runtime/viewengine/ViewEngine.java
@@ -19,12 +19,21 @@
 */
 package com.adeptj.runtime.viewengine;
 
+import static org.trimou.engine.config.EngineConfigurationKey.END_DELIMITER;
+import static org.trimou.engine.config.EngineConfigurationKey.START_DELIMITER;
+import static org.trimou.engine.config.EngineConfigurationKey.TEMPLATE_CACHE_ENABLED;
+import static org.trimou.engine.config.EngineConfigurationKey.TEMPLATE_CACHE_EXPIRATION_TIMEOUT;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.trimou.Mustache;
 import org.trimou.engine.MustacheEngine;
 import org.trimou.engine.MustacheEngineBuilder;
-import org.trimou.engine.config.EngineConfigurationKey;
 import org.trimou.engine.locator.ClassPathTemplateLocator;
 import org.trimou.engine.locator.TemplateLocator;
 import org.trimou.handlebars.Helper;
@@ -60,13 +69,13 @@ public enum ViewEngine {
 	}
 
 	private MustacheEngine mustacheEngine() {
-		Config config = Configs.INSTANCE.common();
+		Config config = Configs.INSTANCE.main().getConfig("trimou");
 		return MustacheEngineBuilder.newBuilder().registerHelper(RB_HELPER_NAME, this.resourceBundleHelper())
 				.addTemplateLocator(templateLocator(config))
-				.setProperty(EngineConfigurationKey.START_DELIMITER, "#{")
-				.setProperty(EngineConfigurationKey.END_DELIMITER, "}")
-				.setProperty(EngineConfigurationKey.TEMPLATE_CACHE_ENABLED, Boolean.TRUE)
-				.setProperty(EngineConfigurationKey.TEMPLATE_CACHE_EXPIRATION_TIMEOUT, 1800).build();
+				.setProperty(START_DELIMITER, config.getString("start-delimiter"))
+				.setProperty(END_DELIMITER, config.getString("end-delimiter"))
+				.setProperty(TEMPLATE_CACHE_ENABLED, config.getBoolean("template-cache-enabled"))
+				.setProperty(TEMPLATE_CACHE_EXPIRATION_TIMEOUT, config.getInt("template-cache-expiration")).build();
 	}
 	
 	ViewEngine() {
@@ -75,26 +84,40 @@ public enum ViewEngine {
 		LoggerFactory.getLogger(ViewEngine.class).info("MustacheEngine initialized in: [{}] ms!!", TimeUnits.nanosToMillis(startTime));
 	}
 
-	public boolean processView(ViewEngineContext context) throws ViewEngineException {
+	public boolean processView(ViewEngineContext context) {
 		long startTime = System.nanoTime();
 		boolean rendered = false;
 		String view = context.getView();
 		LOGGER.debug("Processing view: [{}]", view);
+		HttpServletResponse response = context.getResponse();
 		try {
 			Mustache mustache = this.engine.getMustache(view);
 			if (mustache == null) {
 				LOGGER.info("View: [{}] not found!!", view);
+				// Send error so that container's error page mechanism kicks in.
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
 			} else {
-				context.getResponse().getWriter().write(mustache.render(context.getModels()));
-				rendered = true;
+				response.getWriter().write(mustache.render(context.getModels()));
+			    rendered = true;
 			}
 		} catch (Exception ex) {
 			LOGGER.error("Exception while processing view: [{}]", view, ex);
-			throw new ViewEngineException(ex.getMessage(), ex);
+			this.handleException(context, ex);
 		}
 		if (LOGGER.isDebugEnabled()) {
 			LOGGER.debug("Processed view: [{}] in: [{}] ms!!", view, TimeUnits.nanosToMillis(startTime));	
 		}
 		return rendered;
+	}
+
+	private void handleException(ViewEngineContext context, Exception ex) {
+		context.getRequest().setAttribute(RequestDispatcher.ERROR_EXCEPTION, ex);
+		try {
+			context.getResponse().sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		} catch (IOException ioex) {
+			// Now what? may be log and throw.
+			LOGGER.error("Exception while sending error!!", ex);
+			throw new ViewEngineException(ex.getMessage(), ex);
+		}
 	}
 }
