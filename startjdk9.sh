630224a2e70e908bcc0c59286f3542664f04c576
Rakesh Kumar <kumar_rakesh@live.in>
2017-08-11 15:25:58 +0530

    start options in multi line commands for better readability.

--- startjdk9.sh
+++ startjdk9.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 cd target
-java --add-modules java.se.ee -server -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000 -Dadeptj.rt.port=9007 -Denable.http2=true -Dlog.async=true -Dlog.immediate.flush=true -Dfelix.log.level=3 -jar adeptj-runtime.jar
+java --add-modules java.se.ee -server -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000 \
+ -Dadeptj.rt.port=9007 \
+ -Denable.http2=true \
+ -Dlog.async=true \
+ -Dlog.immediate.flush=true \
+ -Dfelix.log.level=3 \
+ -jar adeptj-runtime.jar
