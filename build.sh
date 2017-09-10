549022aeea7a663f3c8c7ff1aaa347f10026c516
Rakesh Kumar <kumar_rakesh@live.in>
2017-09-10 09:41:56 +0530

    undertow version updated. EclipseLink upgraded to JPA 2.2. Caffeine bundle is installed by default.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn clean package -P Apache-HttpClient,RESTEasy,MySQL,JDBC-CP-Hikari,JPA-EclipseLink
+mvn clean package -P Apache-HttpClient,RESTEasy,AWS,MySQL,JDBC-CP-Hikari,JPA-EclipseLink
