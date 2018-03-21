8c049588246105ba04fc1cc09947d2a427023d4f
Prince Arora <prnc.arora0@gmail.com>
2018-03-21 21:05:21 +0530

    build updated and target added in ignore

--- etc/build.sh
+++ etc/build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-cd ..
-mvn clean package -P Apache-HttpClient,RESTEasy-Jackson,MySQL,JDBC-CP-Hikari,JPA-EclipseLink,AWS
+
+# maven profiles to include support in runtime
+mvn_profiles="Apache-HttpClient,RESTEasy-Jackson,MySQL,JDBC-CP-Hikari,JPA-EclipseLink,AWS"
+
+echo "Building adeptj for profiles - $mvn_profiles"
+
+# executing maven with profiles
+mvn clean package -P $mvn_profiles
