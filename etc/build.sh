dfa0afd23569ccf138f7ea2f502c62f878594d46
Rakesh Kumar <kumar_rakesh@live.in>
2018-03-22 09:46:53 +0530

    updated build, start and stop scripts

--- etc/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# maven profiles to include support in runtime
-mvn_profiles="Apache-HttpClient,RESTEasy-Jackson,MySQL,JDBC-CP-Hikari,JPA-EclipseLink,AWS"
-
-echo "Building adeptj for profiles - $mvn_profiles"
-
-# executing maven with profiles
-mvn clean package -P $mvn_profiles
