3048952f40a6e7d50c8829c900196a22f7183f16
Rakesh Kumar <kumar_rakesh@live.in>
2018-04-07 00:01:52 +0530

    minor refactoring.

--- src/main/java/io/adeptj/runtime/core/Launcher.java
+++ src/main/java/io/adeptj/runtime/core/Launcher.java
@@ -23,7 +23,6 @@ package io.adeptj.runtime.core;
 import com.adeptj.runtime.tools.logging.LogbackManager;
 import com.typesafe.config.Config;
 import io.adeptj.runtime.common.BundleContextHolder;
-import io.adeptj.runtime.common.Constants;
 import io.adeptj.runtime.common.Environment;
 import io.adeptj.runtime.common.ShutdownHook;
 import io.adeptj.runtime.common.Times;
@@ -45,6 +44,7 @@ import static io.adeptj.runtime.common.Constants.ARG_OPEN_CONSOLE;
 import static io.adeptj.runtime.common.Constants.KEY_HTTP;
 import static io.adeptj.runtime.common.Constants.KEY_PORT;
 import static io.adeptj.runtime.common.Constants.OSGI_CONSOLE_URL;
+import static io.adeptj.runtime.common.Constants.REGEX_EQ;
 import static io.adeptj.runtime.common.Constants.SERVER_STOP_THREAD_NAME;
 import static java.util.stream.Collectors.toMap;
 import static org.apache.commons.lang3.SystemUtils.JAVA_RUNTIME_NAME;
@@ -80,48 +80,66 @@ public final class Launcher {
     public static void main(String[] args) {
         Thread.currentThread().setName("AdeptJ Launcher");
         long startTime = System.nanoTime();
-        // First of all initialize Logback.
         LogbackInitializer.init();
         Logger logger = LoggerFactory.getLogger(Launcher.class);
         try {
-            // Useful for debugging the server startup in development mode.
-            if (Environment.isDev()) {
-                Integer waitTime = Integer.getInteger("wait.time.for.debug.attach", 10);
-                logger.info("Waiting [{}] seconds for debugger to attach!", waitTime);
-                TimeUnit.SECONDS.sleep(waitTime);
-            }
+            pauseForDebug();
             logger.info("JRE: [{}], Version: [{}]", JAVA_RUNTIME_NAME, JAVA_RUNTIME_VERSION);
-            Map<String, String> commands = Stream.of(args)
-                    .map(cmd -> cmd.split(Constants.REGEX_EQ))
-                    .collect(toMap(cmdArray -> cmdArray[0], cmdArray -> cmdArray[1]));
-            logger.debug("Commands to AdeptJ Runtime: {}", commands);
+            Map<String, String> commands = parseArgs(args);
             Server server = new Server();
             server.start();
             Runtime.getRuntime().addShutdownHook(new ShutdownHook(server, SERVER_STOP_THREAD_NAME));
-            if (Boolean.parseBoolean(commands.get(ARG_OPEN_CONSOLE))) {
-                try {
-                    Config config = Configs.DEFAULT.undertow().getConfig(KEY_HTTP);
-                    Environment.launchBrowser(new URL(String.format(OSGI_CONSOLE_URL, config.getInt(KEY_PORT))));
-                } catch (IOException ex) {
-                    // Just log it, its okay if browser is not launched.
-                    logger.error("Exception while launching browser!!", ex);
-                }
-            }
+            launchBrowser(commands);
             logger.info("AdeptJ Runtime initialized in [{}] ms!!", Times.elapsedMillis(startTime));
         } catch (Throwable th) { // NOSONAR
             logger.error("Exception while initializing AdeptJ Runtime!!", th);
-            if (Boolean.getBoolean(SYS_PROP_ENABLE_SYSTEM_EXIT)) {
-                // Check if OSGi Framework was already started, try to stop the framework gracefully.
-                Optional.ofNullable(BundleContextHolder.INSTANCE.getBundleContext())
-                        .ifPresent(context -> {
-                            logger.warn("Server startup failed but OSGi Framework was started already, stopping it gracefully!!");
-                            FrameworkManager.INSTANCE.stopFramework();
-                        });
-                logger.error("Shutting down JVM!!", th);
-                // Let the LOGBACK cleans up it's state.
-                LogbackManager.INSTANCE.getLoggerContext().stop();
-                System.exit(-1);
+            shutdownJvm(th);
+        }
+    }
+
+    private static void pauseForDebug() throws InterruptedException {
+        // Useful for debugging the server startup in development mode.
+        if (Environment.isDev()) {
+            Integer waitTime = Integer.getInteger("wait.time.for.debug.attach", 10);
+            LoggerFactory.getLogger(Launcher.class)
+                    .info("Waiting [{}] seconds for debugger to attach!", waitTime);
+            TimeUnit.SECONDS.sleep(waitTime);
+        }
+    }
+
+    private static Map<String, String> parseArgs(String[] args) {
+        Map<String, String> commands = Stream.of(args)
+                .map(cmd -> cmd.split(REGEX_EQ))
+                .collect(toMap(cmdArray -> cmdArray[0], cmdArray -> cmdArray[1]));
+        LoggerFactory.getLogger(Launcher.class).debug("Commands to AdeptJ Runtime: {}", commands);
+        return commands;
+    }
+
+    private static void launchBrowser(Map<String, String> commands) {
+        if (Boolean.parseBoolean(commands.get(ARG_OPEN_CONSOLE))) {
+            try {
+                Config config = Configs.DEFAULT.undertow().getConfig(KEY_HTTP);
+                Environment.launchBrowser(new URL(String.format(OSGI_CONSOLE_URL, config.getInt(KEY_PORT))));
+            } catch (IOException ex) {
+                // Just log it, its okay if browser is not launched.
+                LoggerFactory.getLogger(Launcher.class).error("Exception while launching browser!!", ex);
             }
+        }
+    }
+
+    private static void shutdownJvm(Throwable th) {
+        if (Boolean.getBoolean(SYS_PROP_ENABLE_SYSTEM_EXIT)) {
+            Logger logger = LoggerFactory.getLogger(Launcher.class);
+            // Check if OSGi Framework was already started, try to stop the framework gracefully.
+            Optional.ofNullable(BundleContextHolder.INSTANCE.getBundleContext())
+                    .ifPresent(context -> {
+                        logger.warn("Server startup failed but OSGi Framework already started, stopping it gracefully!!");
+                        FrameworkManager.INSTANCE.stopFramework();
+                    });
+            logger.error("Shutting down JVM!!", th);
+            // Let the LOGBACK cleans up it's state.
+            LogbackManager.INSTANCE.getLoggerContext().stop();
+            System.exit(-1);
         }
     }
 }
