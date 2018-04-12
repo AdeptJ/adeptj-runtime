bebf484c41aa108a00f75b51692b4a9e99265b5d
Rakesh Kumar <kumar_rakesh@live.in>
2018-04-12 11:03:57 +0530

    removed redundant cache profiles.

--- etc/build.sh
+++ etc/build.sh
@@ -4,13 +4,13 @@ GREEN=$(echo -en '\033[00;32m')
 RESTORE=$(echo -en '\033[0m')
 
 # maven profiles to include support in runtime
-MVN_PROFILES="Apache-HttpClient,RESTEasy,Jackson,MySQL,JDBC-CP-Hikari,JPA-EclipseLink,AWS,Ehcache,ReactiveStreams"
+MVN_PROFILES="Apache-HttpClient,RESTEasy,Jackson,ReactiveStreams"
 echo ${GREEN}
-echo -e "########################################################################################################################################"
-echo -e "# Building AdeptJ Runtime with following maven profiles                                                                                #"
+echo -e "######################################################################################"
+echo -e "# Building AdeptJ Runtime with following maven profiles                              #"
 echo -e "# [$MVN_PROFILES]"
-echo -e "# Please select more profiles from pom.xml to add to the build process, if required!                                                   #"
-echo -e "########################################################################################################################################"
+echo -e "# Please select more profiles from pom.xml to add to the build process, if required! #"
+echo -e "######################################################################################"
 echo ${RESTORE}
 
 # executing maven with profiles
