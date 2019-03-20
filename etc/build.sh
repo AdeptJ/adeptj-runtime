fc79e34d47ed7b7517a65e53d80c270c2910abc4
Rakesh Kumar <kumar_rakesh@live.in>
2019-03-20 10:46:53 +0530

    no shade profile removed.

--- etc/build.sh
+++ etc/build.sh
@@ -4,7 +4,7 @@ GREEN=$(echo -en '\033[00;32m')
 RESTORE=$(echo -en '\033[0m')
 
 # maven profiles to include support in runtime
-MVN_PROFILES="no-shade,ApacheHttpClient,HibernateValidator,RESTEasy,Jackson,JWT,ReactiveStreams,JDBC-CP-Hikari,JPA-EclipseLink,MySQL,OAuth2,Email"
+MVN_PROFILES="shade,ApacheHttpClient,HibernateValidator,RESTEasy,Jackson,JWT,ReactiveStreams,JDBC-CP-Hikari,JPA-EclipseLink,MySQL,OAuth2,Email"
 echo ${GREEN}
 echo -e "######################################################################################"
 echo -e "# Building AdeptJ Runtime with following maven profiles                              #"
