e95192930fe7799c68862be9bec00bdbe58f0c09
Rakesh Kumar <kumar_rakesh@live.in>
2019-08-13 19:29:28 +0530

    Dependencies version updated.

--- etc/build.sh
+++ etc/build.sh
@@ -5,13 +5,13 @@ RESTORE=$(echo -en '\033[0m')
 
 # maven profiles to include support in runtime
 MVN_PROFILES="no-shade,ApacheHttpClient,HibernateValidator,RESTEasy,Jackson,JWT,ReactiveStreams,JDBC-CP-Hikari,JPA-EclipseLink,MySQL,OAuth2,Email"
-echo ${GREEN}
+echo "${GREEN}"
 echo -e "######################################################################################"
 echo -e "# Building AdeptJ Runtime with following maven profiles                              #"
 echo -e "# [$MVN_PROFILES]"
 echo -e "# Please select more profiles from pom.xml to add to the build process, if required! #"
 echo -e "######################################################################################"
-echo ${RESTORE}
+echo "${RESTORE}"
 
 # executing maven with profiles
 mvn clean package -P ${MVN_PROFILES}
