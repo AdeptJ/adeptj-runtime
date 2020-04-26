480036272c7bb875afe70d89310e2919fb244605
Rakesh Kumar <kumar_rakesh@live.in>
2020-04-26 14:05:42 +0530

    ErrorServlet now only consider status set by the container.

--- src/main/java/com/adeptj/runtime/servlet/ErrorServlet.java
+++ src/main/java/com/adeptj/runtime/servlet/ErrorServlet.java
@@ -26,14 +26,11 @@ import com.adeptj.runtime.config.Configs;
 import com.adeptj.runtime.templating.TemplateData;
 import com.adeptj.runtime.templating.TemplateEngine;
 import com.adeptj.runtime.templating.TemplateEngineContext;
-import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import static com.adeptj.runtime.common.Constants.SLASH;
 
 /**
  * ErrorServlet that serves the error page w.r.t status(401, 403, 404, 500).
@@ -45,28 +42,22 @@ public class ErrorServlet extends HttpServlet {
 
     private static final long serialVersionUID = -3339904764769823449L;
 
-    private static final String STATUS_500 = "500";
-
     private static final String KEY_EXCEPTION = "exception";
-
-    private static final String TEMPLATE_500 = "error/500";
 
     private static final String KEY_STATUS_CODES = "common.status-codes";
 
-    private static final String TEMPLATE_ERROR_FMT = "error/%s";
+    private static final String ERROR_TEMPLATE_FMT = "error/%s";
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
-        String status = StringUtils.substringAfterLast(req.getRequestURI(), SLASH);
-        if (StringUtils.equals(STATUS_500, status)) {
-            TemplateEngineContext.Builder builder = TemplateEngineContext.builder(TEMPLATE_500, resp);
+        int status = resp.getStatus();
+        if (Configs.of().undertow().getIntList(KEY_STATUS_CODES).contains(status)) {
+            String template = String.format(ERROR_TEMPLATE_FMT, status);
+            TemplateEngineContext.Builder builder = TemplateEngineContext.builder(template, resp);
             if (Environment.isDev() && RequestUtil.hasException(req)) {
                 builder.templateData(new TemplateData().with(KEY_EXCEPTION, RequestUtil.getException(req)));
             }
             TemplateEngine.getInstance().render(builder.build());
-        } else if (Configs.of().undertow().getStringList(KEY_STATUS_CODES).contains(status)) {
-            TemplateEngine.getInstance()
-                    .render(TemplateEngineContext.builder(String.format(TEMPLATE_ERROR_FMT, status), resp).build());
         }
     }
 }
