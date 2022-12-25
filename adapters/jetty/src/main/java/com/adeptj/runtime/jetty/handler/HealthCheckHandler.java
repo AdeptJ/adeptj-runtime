14e8dd1ec1e31852c8104bdeea8a83c020df3e29
Rakesh Kumar <kumar_rakesh@live.in>
2022-12-25 20:10:09 +0530

    Removed extra health check path.
    /hc is standard all across.

--- adapters/jetty/src/main/java/com/adeptj/runtime/jetty/handler/HealthCheckHandler.java
+++ adapters/jetty/src/main/java/com/adeptj/runtime/jetty/handler/HealthCheckHandler.java
@@ -1,11 +1,11 @@
 package com.adeptj.runtime.jetty.handler;
 
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.HandlerWrapper;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.HandlerWrapper;
+
 import java.io.IOException;
 
 public class HealthCheckHandler extends HandlerWrapper {
@@ -13,7 +13,7 @@ public class HealthCheckHandler extends HandlerWrapper {
     @Override
     public void handle(String target, Request baseRequest,
                        HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getRequestURI().equals("/hc") || request.getRequestURI().equals("/health")) {
+        if (request.getRequestURI().equals("/hc")) {
             response.setStatus(HttpServletResponse.SC_OK);
             baseRequest.setHandled(true);
             return;
